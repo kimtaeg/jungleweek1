c5e589e901724421c05bd929ff0f0e0a4641e271
kimtaeg <tank_1017@naver.com>
2026-08-25 10:48:38 +0900

    refactor: 회원가입 UI 수정 및 추가

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+if __name__ == '__main__':
+    app.run(debug=True)
